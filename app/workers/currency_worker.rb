8726fdd418f7d1dfd5418aaeb811a043a13add36
Mateusz Tuński <mateusz@tunski.co>
2015-05-03 20:07:38 +0200

    Implement Currency worker and tests

---
+++ app/workers/currency_worker.rb
@@ -0,0 +1,21 @@
+class CurrencyWorker
+  include Sneakers::Worker
+  from_queue "currencies.queue_#{ENV['QUEUE_ID']}"
+
+  def work(msg)
+    currency = update_or_create_currency(JSON.parse(msg, symbolize_names: true))
+
+    AcknowledgementPublisher.new(currency.id).call && ack!
+  end
+
+  private
+
+  def update_or_create_currency(currency_json)
+    Currency.find_or_initialize_by(currency_json[:uuid]).tap do |currency|
+      currency.rates = currency_json[:rates]
+      currency.save!
+    end
+  end
+
+  attr_accessor :retries
+end
---
+++ test/workers/currency_worker_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+require 'mocha/mini_test'
+
+class CurrencyWorkerTest < ActiveSupport::TestCase
+  setup do
+    @currency_worker = CurrencyWorker.new
+    AcknowledgementPublisher.any_instance.stubs(:call).returns(true)
+  end
+
+  def test_creates_new_currency_if_not_already_in_the_db
+    assert_difference 'Currency.count' do
+      @currency_worker.work({ id: 'uuid', rates: { eur: 1 } }.to_json)
+    end
+  end
+
+  def test_does_not_create_new_currency_if_already_in_the_db
+    currency = Currency.create!(id: 'uuid', rates: { eur: 666 })
+
+    assert_no_difference 'Currency.count' do
+      @currency_worker.work({ id: 'uuid', rates: { eur: 1 } }.to_json)
+    end
+
+    currency.reload
+
+    assert_equal ({ 'eur' => 1 }), currency.rates
+  end
+
+  def test_acks_when_successfull
+    @currency_worker.expects(:ack!).once
+
+    @currency_worker.work({ id: 'uuid', rates: { eur: 1 } }.to_json)
+  end
+end
